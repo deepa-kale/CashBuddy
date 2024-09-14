9ace82b37eef737e8b573f199cf81d26927ac882
Deepa Kale <deepakale@Deepas-MacBook-Pro.local>
2024-09-14 15:29:57 -0500

    first commit

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,15 @@
+// app/auth/sign-in/page.tsx
+
+import React from 'react';
+
+// Define the SignIn component as a functional component
+const SignIn: React.FC = () => {
+  return (
+    <div>
+      <h1>SignIn</h1>
+    </div>
+  );
+};
+
+// Export the component as default
+export default SignIn;
